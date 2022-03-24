b491a4779fd5175c764537b9b5b90bbed5871f82
p0lich <a.polikarpov2014@gmail.com>
2022-03-24 13:53:42 +0400

    add project menu script, edit styles

--- task03_ResponsiveCSS/scripts/toggle_info.js
+++ task03_ResponsiveCSS/scripts/toggle_info.js
@@ -1,9 +1,18 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
+function displayInfo(x) {
+    x.classList.toggle("fa-circle-minus");
+
+    if (window.screen.width >= 480) {
+        document.getElementById("project_details").style.height = "90px";
+        document.getElementById("person_block").style.marginTop = "15px";
+    } else {
+        document.getElementById("project_details").style.height = "150px";
+        document.getElementById("person_block").style.marginTop = "10px";
+    }
+
+    
+}
   
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+function hideInfo() {
+    // document.getElementById("mySidebar").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+}
