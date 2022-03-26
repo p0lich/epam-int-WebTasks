7fd9d874d79660ccb0d2937dd0ecadcf7e752dd8
p0lich <a.polikarpov2014@gmail.com>
2022-03-26 21:11:02 +0400

    change colors, fix scripts

--- task03_ResponsiveCSS/scripts/toggle_info.js
+++ task03_ResponsiveCSS/scripts/toggle_info.js
@@ -5,12 +5,10 @@ function displayInfo(x) {
     var bottom_block = document.getElementById("person_block");
 
     if (window.screen.width >= 480) {
-        top_block.style.height = (top_block.style.height == "100%") ? "0" : "100%";
+        top_block.style.height = (top_block.style.height == "115px") ? "0" : "115px";
         bottom_block.style.marginTop = (bottom_block.style.marginTop == "15px") ? "0" : "15px";
     } else {
-        top_block.style.height = (top_block.style.height == "100%") ? "0" : "100%";
+        top_block.style.height = (top_block.style.height == "150px") ? "0" : "150px";
         bottom_block.style.marginTop = (bottom_block.style.marginTop == "10px") ? "0" : "10px";
     }
-
-    
 }
