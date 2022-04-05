d6c8fde632c4c624e1a29b0e4c0a8d9e0f97eec7
p0lich <a.polikarpov2014@gmail.com>
2022-04-05 15:47:55 +0400

    add script for repeted letters remove

--- task04_JavaScript/scripts/letter_remover.js
+++ task04_JavaScript/scripts/letter_remover.js
@@ -1,20 +1,58 @@
 function removeLetters() {
     let text = document.getElementById('text_input').value;
-    let splitArray = splitString(text);
 
-    let temp = removeDuplicates(splitArray.join(''));
+    let splitArray = str.split(/[ \.\?,;:!]/);
+    let wordsCount = getWordsCount(splitArray);
+
+    let lettersMap = getLettersMap(text);
     
-    for (let i = 0; i < splitArray.length; i++) {
-        splitArray[i] = removeDuplicates(splitArray[i]);
+    let letterRepeatMap = calculateLettersRepeatCount(splitArray, lettersMap);
+
+    let newText = "";
+    for(let i = 0; i < text.length; i++) {
+        if(wordsCount > 0 && letterRepeatMap.get(text[i]) === wordsCount) {
+            continue;
+        }
+        newText += text[i];
     }
 
-    document.getElementById('text_output').value = splitArray.join('');
+    document.getElementById('text_output').value = newText;
 }
 
-function splitString(str) {
-    return str.split(/[ \.\?,;:!]/);
+function getWordsCount(splitString) {
+    let count = 0;
+    for(let i = 0; i < splitString.length; i++) {
+        if(splitString[i].length > 0) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
-function removeDuplicates(str) {
-    return [...new Set(str)].join('');
+function getLettersMap(str) {
+    let stringLetters = new Set(str);
+    let lettersMap = new Map();
+
+    for(let letter of stringLetters) {
+        lettersMap.set(letter, 0);
+    }
+
+    return lettersMap;
+}
+
+function calculateLettersRepeatCount(splitString, lettersMap) {
+    for(let i = 0; i < splitString.length; i++) {
+        if(splitString[i].length == 0) {
+            continue;
+        }
+
+        let wordSet = new Set(splitString[i]);
+
+        for (let letter of wordSet) {
+            lettersMap.set(letter, lettersMap.get(letter) + 1);
+        }
+    }
+
+    return lettersMap;
 }
